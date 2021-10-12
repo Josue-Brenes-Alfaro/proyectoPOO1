66915288b001a25e932811655cdebb4cd2de2356
Josué Brenes Alfaro <65695014+Josue-Brenes-Alfaro@users.noreply.github.com>
2021-10-11 18:32:18 -0600

    clases

--- ProyectoPOO1/src/main/java/logicadenegocios/Cursos.java
+++ ProyectoPOO1/src/main/java/logicadenegocios/Cursos.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package logicadenegocios;
 
 /**
@@ -9,5 +5,5 @@ package logicadenegocios;
  * @author Josue
  */
 public class Cursos {
-  
+
 }
