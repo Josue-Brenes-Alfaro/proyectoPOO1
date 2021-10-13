a7f6a81c900f796606f91aefe8987264d9f92e54
FarolaynTEC <farolayn@estudiantec.cr>
2021-10-12 21:22:46 -0600

    Creacion de la clase bloque
    Aqui agregue un constructor jijiji

--- ProyectoPOO1/src/main/java/logicadenegocios/Bloque.java
+++ ProyectoPOO1/src/main/java/logicadenegocios/Bloque.java
@@ -6,4 +6,31 @@ package logicadenegocios;
  */
 public class Bloque {
 
+  private String curso;
+  private int id;
+
+  /**
+   * 
+   * @param curso
+   * @param id 
+   */
+  public Bloque(String curso, int id) {
+    this.curso = curso;
+    this.id = id;
+  }
+
+  public String getCurso() {
+    return curso;
+  }
+
+  public void setCurso(String curso) {
+    this.curso = curso;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+  
+  
+  
 }
