66c5c678d6d3113397933b50ce97d5608967e7b2
Josué Brenes Alfaro <65695014+Josue-Brenes-Alfaro@users.noreply.github.com>
2021-10-15 15:57:05 -0600

    Inicio de la conexion SQL

---
+++ Proyecto_POO1/src/SQL/Conexion.java
@@ -0,0 +1,24 @@
+package SQL;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+/**
+ *
+ * @author Josue
+ */
+public class Conexion {
+  public static Connection getConexion(){
+    String conexionURL = "jbdc:sqlserver:localhost\\SQLEXPRESS"
+        +"database=Proyecto_POO1"
+        +"loginTimeout=30";
+    
+    try{
+      Connection con = DriverManager.getConnection(conexionURL);
+      return con;
+    }catch(SQLException ex){
+      System.out.println(ex.toString());
+      return null;
+    }
+  }
+  
+}
