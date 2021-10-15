4d5a653518631045376360f87b8a5b106507f65c
Josué Brenes Alfaro <65695014+Josue-Brenes-Alfaro@users.noreply.github.com>
2021-10-14 23:04:31 -0600

    Base de datos SQL

---
+++ Base de datos SQL.sql
@@ -0,0 +1,43 @@
+--Creación de la base de dato Proyecto_POO1
+CREATE DATABASE Proyecto_POO1
+--Se coloca en la base de datos Proyecto_POO1 para hacerle modificaciones
+USE Proyecto_POO1
+
+CREATE TABLE EscuelaOArea(
+  codigoCarrera varchar(20) NOT NULL,
+  nombreCarrera varchar(70) NOT NULL,
+  tieneEscuea bit NOT NULL,
+  planDeEstudio varchar(10) NOT NULL,
+  idPlan varchar(10),
+  PRIMARY KEY (codigoCarrera),
+  FOREIGN KEY (idPlan) REFERENCES PlanDeEstudio(idPlan)
+);
+
+CREATE TABLE PlanDeEstudio(
+  idPlan varchar(10) NOT NULL,
+  fechaVigencia datetime NOT NULL,
+  cantidadCursos int NOT NULL,
+  cantidadCreditos int NOT NULL,
+  codigoCurso varchar(10) NOT NULL,
+  PRIMARY KEY (idPlan),
+  FOREIGN KEY (codigoCurso) REFERENCES Curso(codigoCurso)
+);
+
+CREATE TABLE Curso(
+  codigoCurso varchar(10) NOT NULL,
+  nombreCurso varchar(70) NOT NULL,
+  cantidadCreditos int NOT NULL,
+  horasLectivas int NOT NULL,
+  numBloque int NOT NULL,
+  cantidadCursos int NOT NULL,
+  PRIMARY KEY (codigoCurso)
+);
+
+CREATE TABLE Requisito(
+  nombreCurso varchar(10),
+  cursoRequisito varchar(10),
+  cursoCorrequisito varchar(10),
+  FOREIGN KEY (nombreCurso) REFERENCES Curso(codigoCurso),
+  FOREIGN KEY (cursoRequisito) REFERENCES Curso(codigoCurso),
+  FOREIGN KEY (cursoCorrequisito) REFERENCES Curso(codigoCurso)
+);
