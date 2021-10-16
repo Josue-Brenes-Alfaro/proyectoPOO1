f3605a2c019ae2136c108eaca2b5871f108ea0f7
Josué Brenes Alfaro <65695014+Josue-Brenes-Alfaro@users.noreply.github.com>
2021-10-15 21:31:53 -0600

    Conexion bd3.0

--- Proyecto_POO1/src/SQL/Conexion.java
+++ Proyecto_POO1/src/SQL/Conexion.java
@@ -1,27 +1,28 @@
 package SQL;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 /**
  *
  * @author Josue
  */
 public class Conexion {
+  
   public static Connection getConexion(){
-    String conexionURL = "jbdc:sqlserver://localhost\\SQLEXPRESS"
-        +"database=Proyecto_POO1"
-        +"user="
-        +"password="
-        +"loginTimeout=30";
-
-    try{
-      Connection con = DriverManager.getConnection(conexionURL);
-      return con;
-    }catch(SQLException ex){
-      System.out.println(ex.toString());
+    try {
+      Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+      String connectionUrl = "jbdc:sqlserver://localhost\\SQLEXPRESS:1433;databaseName=Proyecto_POO1;IntegratedSecurity=true";
+      Connection conect = DriverManager.getConnection(connectionUrl);
+      System.out.println("Conectado.");
+      return conect;
+    } 
+      catch (SQLException ex){
+      System.out.println("Error.");
+      return null;
+    } catch (ClassNotFoundException ex){
+      Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
       return null;
     }
   }
-  
 }
