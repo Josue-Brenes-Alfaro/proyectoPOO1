49e8ef2b094fa464ae6e82fe2b652fc40f311f2a
97ale <merino.ale97@estudiantec.cr>
2021-10-17 21:17:32 -0600

    Elimina clase Requisito. Actualiza clase Curso.

--- Proyecto_POO1/src/logicadenegocios/Curso.java
+++ Proyecto_POO1/src/logicadenegocios/Curso.java
@@ -1,7 +1,10 @@
 package logicadenegocios;
 
+import excepciones.CursoDoesNotExistsException;
+import interfaces.InterfaceCurso;
+import java.util.ArrayList;
 import logicadenegocios.EscuelaOArea; 
-import logicadenegocios.Requisito; 
+ 
 /**
  * Esta es una abstraccion de la clase Cursos con fines academicos
  * @author Alejandra Merino
@@ -9,7 +12,7 @@ import logicadenegocios.Requisito;
  * @author Josué Brenes
  * @version 1.0
  */
-public class Curso {
+public class Curso implements InterfaceCurso{
   private String codigoCurso;
   private String nombreCurso;
   private int cantidadCreditos;
@@ -17,7 +20,8 @@ public class Curso {
   private int numBloque;
   private static int cantidadCursos=0;
   private EscuelaOArea escuelaOAreaACargo;
-  private Requisito requisito;
+  private ArrayList<Curso> cursosRequisitos;
+  private ArrayList<Curso> cursosCorrequisitos;
   
   /**
    * Metodo constructor de objetos de la clase Curso
@@ -36,42 +40,100 @@ public class Curso {
     this.cantidadCreditos++;
   }
   
-  /**
-   * Metodo que agrega requisitos a un objeto de la clase Curso
-   * @param pRequisito 
-   */
-  public void agregarRequisito (Requisito pRequisito) {
-    this.requisito = pRequisito;
+  public void eliminarRequisito(String pCodigoCurso){
+  //aqui se debe eliminar el curso, pero primero hay que buscar si existe.
+  //lanzaria un error si no existe.
+  }
+  public Curso buscarCurso (String pCodigoCurso) throws CursoDoesNotExistsException {
+    for (Curso cursoActual: cursosRequisitos) {
+      if(cursoActual.getCodigoCurso().equals(pCodigoCurso))
+        return cursoActual;
+    } throw new CursoDoesNotExistsException(pCodigoCurso);
   }
   
-  /**
-   * Metodo que agrega un numero de bloque a objetos de la clase Curso
-   * @param pNumBloque 
-   */
-  public void agregarNumBloque (int pNumBloque) {
-    this.numBloque = pNumBloque;
+  public boolean existeCurso (String pCodigoCurso){
+    for(Curso cursoActual: cursosRequisitos) {
+      if(cursoActual.getCodigoCurso().equals(pCodigoCurso))
+        return true;
+    } return false;
+  }
+  
+  public Curso buscarCursoCorrequisito (String pCodigoCurso) throws CursoDoesNotExistsException {
+    for (Curso cursoActual: cursosCorrequisitos) {
+      if(cursoActual.getCodigoCurso().equals(pCodigoCurso))
+        return cursoActual;
+    } throw new CursoDoesNotExistsException(pCodigoCurso);
+  }
+  
+  public boolean existeCursoCorrequisito (String pCodigoCurso){
+    for(Curso cursoActual: cursosCorrequisitos) {
+      if(cursoActual.getCodigoCurso().equals(pCodigoCurso))
+        return true;
+    } return false;
   }
 
   /**
-   * 
+   * Meotdo que imprime un mensaje con la informacion de un curso.
    * @return cadena como String
    */
   public String toString() {
     String cadena = "";
-    cadena = "Cédigo del curso: " + codigoCurso + "\n";
+    cadena = "Código del curso: " + codigoCurso + "\n";
     cadena+= "Nombre del curso: " + nombreCurso + "\n";
     cadena+= "Cantidad de créditos: " + cantidadCreditos + "\n";
     cadena+= "Horas lectivas: " + horasLectivas + "\n";
-    cadena+= "Némero de bloque: " + numBloque + "\n";
-    cadena+= "Escuela o Area Académica a cargo: " + numBloque + "\n";        
-    cadena+= "Requisitos: " + requisito + "\n";
-    
+    cadena+= "Número de bloque: " + numBloque + "\n";
+    cadena+= "Escuela o Area Académica a cargo: " + numBloque + "\n";
+    cadena+= "Cursos requisitos: \n";
+    for(Curso cursoImprime: cursosRequisitos){
+      cadena+= "Codigo curso: " + cursoImprime.getCodigoCurso() +"\n";
+      cadena+= "Nombre curso: " + cursoImprime.getNombreCurso()+"\n";
+    }
+    cadena+= "Cursos correquisitos: \n";
+    for(Curso cursoImprime: cursosCorrequisitos){
+      cadena+= "Codigo curso: " + cursoImprime.getCodigoCurso() +"\n";
+      cadena+= "Nombre curso: " + cursoImprime.getNombreCurso()+"\n";
+    }
     return cadena;
   }
 
   //Metodos accesores
+  public void setCursoRequisito(Curso pRequisito) {
+    this.cursosRequisitos.add(pRequisito);
+  }
+  public void setCursoCorrerequisito (Curso pCorrequisito) {
+    this.cursosCorrequisitos.add(pCorrequisito);
+  }
+  public void setNumBloque (int pNumBloque) {
+    this.numBloque = pNumBloque;
+  }
+  public void setCantidadCreditos(int pCantidadCreditos) {
+    this.cantidadCreditos = pCantidadCreditos;
+  }
+  public void setHorasLectivas(int pHorasLectivas) {
+    this.horasLectivas = pHorasLectivas;
+  }
+  public void setNombreCurso(String pNombreCurso) {
+    this.nombreCurso = pNombreCurso;
+  }
+  
+  public int getNumBloque() {
+    return numBloque;
+  }
+  public String getNombreCurso() {
+    return nombreCurso;
+  }
+  public int getCantidadCreditos() {
+    return cantidadCreditos;
+  }
+  public int getHorasLectivas() {
+    return horasLectivas;
+  }
+  public static int getCantidadCursos() {
+    return cantidadCursos;
+  }
+ 
   public String getCodigoCurso() {
     return codigoCurso;
   }
-  
 }
